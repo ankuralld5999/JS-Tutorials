4ea1dd3b6bf20a982945a0e5673a52665b6fbb01
Ankur Singh <63892190+ankuralld5999@users.noreply.github.com>
2024-02-27 15:21:43 +0000

    Adding 01_variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 15656
+let accountEmail = "ankuralld5999@gmail.com"
+var accountPassord = "India"
+accountCity = "Noida"
+let accountState
+
+
+// const accountNumber
+// accountId = 1256  |=> constant variables can't be modified it meants to be constant
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+accountEmail = "ankit@tcs.com"
+accountPassord = "Basti"
+accountCity = "Gorakhpur"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassord, accountCity, accountState])
