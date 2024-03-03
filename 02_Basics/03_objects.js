94c4494cc2d3156dc0868377eef0ca6206a75f7c
Ankur Singh <63892190+ankuralld5999@users.noreply.github.com>
2024-03-03 05:01:55 +0000

    Objects part 1, symbol function in objects

---
+++ 02_Basics/03_objects.js
@@ -0,0 +1,38 @@
+// singleton
+// Object.create
+
+// object literals
+
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Hitesh",
+    "full name": "Hitesh Choudhary",
+    [mySym]: "mykey1", // symbol can be used as key in [] brackets
+    age: 18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])  // we can't access with . operator
+// console.log(JsUser[mySym]) // we can use symbols in [] brackets
+
+JsUser.email = "hitesh@chatgpt.com"
+// Object.freeze(JsUser)  //after freezing the object values does not change, but it won't reflect any error while changing value after freezing just, it won't reflect in result
+JsUser.email = "hitesh@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
